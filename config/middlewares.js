f741807dcc51ee0de77ab295b79dd26962bce3e4
DaniDeveloper11 <151704893+DaniDeveloper11@users.noreply.github.com>
2026-07-19 11:19:25 -0600

    latest

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,31 @@
+const FRONTEND_ORIGINS = [
+  'http://localhost:3000',
+  'https://mandaditoz-front-production.up.railway.app',
+]
+
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src':     ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io'],
+          'media-src':   ["'self'", 'data:', 'blob:'],
+          'frame-ancestors': ["'self'", ...FRONTEND_ORIGINS],
+          upgradeInsecureRequests: null,
+        },
+      },
+      frameguard: false,
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
-      origin: [
-        'http://localhost:3000',
-        'https://mandaditoz-front-production.up.railway.app',
-      ],
+      origin: FRONTEND_ORIGINS,
       headers: '*',
       credentials: true,
     },
